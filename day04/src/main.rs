bd4146b6ff5956a9720c720dbe011d9b93ea701f
timothyscherer <tjtractorboy@gmail.com>
2023-12-10 15:39:41 +0000

    Add part2 functionality and fix bugs in part1

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let input = include_str!("input.txt");
     println!("part1 = {}", part1::run(input));
+    println!("part2 = {}", part2::run(input));
 }
 
 #[test]
@@ -9,6 +10,7 @@ fn part1() {
     assert_eq!(
         part1::parse(input.lines().next().unwrap()),
         part1::Card {
+            id: 1,
             winners: vec![41, 48, 83, 86, 17],
             players: vec![83, 86, 6, 31, 17, 9, 48, 53],
         }
@@ -16,33 +18,54 @@ fn part1() {
     assert_eq!(part1::run(input), 13);
 }
 
+#[test]
+fn part2() {
+    let input = include_str!("example1.txt");
+    assert_eq!(part2::run(input), 30);
+}
+
 mod part1 {
-    #[derive(Debug, PartialEq)]
+    #[derive(Clone, Debug, PartialEq)]
     pub struct Card {
+        pub id: usize,
         pub winners: Vec<i32>,
         pub players: Vec<i32>,
     }
 
     pub fn run(input: &str) -> i32 {
-        input.lines().map(parse).map(score).sum()
+        input.lines().map(parse).map(|card| score(&card)).sum()
     }
 
-    pub fn score(card: Card) -> i32 {
-        let mut score = 0;
+    pub fn count(card: &Card) -> i32 {
+        let mut count = 0;
         for &player in card.players.iter() {
             if card.winners.contains(&player) {
-                score += 1;
+                count += 1;
             }
         }
+        count
+    }
+
+    pub fn score(card: &Card) -> i32 {
+        let score = count(card);
         if score < 1 {
-            return 0;
+            0
+        } else {
+            (2 as i32).pow(score as u32 - 1)
         }
-        (2 as i32).pow(score - 1)
     }
 
     pub fn parse(input: &str) -> Card {
         let mut parts = input.split(':');
-        let _ = parts.next().unwrap();
+        let id = parts
+            .next()
+            .unwrap()
+            .split(' ')
+            .filter(|s| !s.is_empty() && *s != "Card")
+            .next()
+            .unwrap()
+            .parse::<usize>()
+            .unwrap();
         let mut raw = parts.next().unwrap().split('|');
         let winners = raw
             .next()
@@ -58,6 +81,32 @@ mod part1 {
             .filter(|s| !s.is_empty())
             .map(|s| s.trim().parse::<i32>().unwrap())
             .collect();
-        Card { winners, players }
+        Card {
+            id,
+            winners,
+            players,
+        }
+    }
+}
+
+mod part2 {
+    use crate::part1::{self, Card};
+
+    pub fn run(input: &str) -> i32 {
+        let mut winners = input
+            .lines()
+            .map(part1::parse)
+            .collect::<Vec<Card>>()
+            .iter()
+            .map(|card| part1::count(&card))
+            .collect::<Vec<i32>>();
+
+        for i in (0..winners.len()).rev() {
+            for j in 1..=winners[i] as usize {
+                winners[i] += winners[i + j];
+            }
+        }
+
+        winners.iter().sum::<i32>() + winners.len() as i32
     }
 }
